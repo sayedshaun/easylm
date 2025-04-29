8d83cbb7985edc27de9396f8474de7834bd4f95d
sayedshaun <sayedshaun4@gmail.com>
2025-04-29 16:00:43 +0600

    trainer now supports dp, ddp

--- easylm/trainer.py
+++ easylm/trainer.py
@@ -3,9 +3,9 @@ import shutil
 import yaml
 import torch
 import wandb
+import warnings
 from tqdm import tqdm
 from typing import Any, Union
-from torch.utils.data import DataLoader
 from dataclasses import asdict
 from torch.amp import autocast, GradScaler
 from torch.utils.data import DataLoader
@@ -15,14 +15,14 @@ from easylm.tokenizer import Tokenizer
 
 class Trainer:
     def __init__(
-            self, 
-            model: torch.nn.Module, 
-            config: TrainingConfig,
-            tokenizer: Tokenizer,
-            optimizer: Union[torch.optim.Optimizer, None] = None, 
-            model_name: str = "my_pretrained_model",
-            collate_fn: Union[None, callable] = None,
-    ) -> None:
+        self, 
+        model: torch.nn.Module, 
+        config: TrainingConfig,
+        tokenizer: Tokenizer,
+        optimizer: Union[torch.optim.Optimizer, None] = None, 
+        model_name: str = "my_pretrained_model",
+        collate_fn: Union[None, callable] = None,
+        ) -> None:
         self.config = config
         self.model = model
         self.device = config.device
@@ -52,9 +52,10 @@ class Trainer:
         self.shuffle_train_data = config.shuffle_data
         self.pin_memory = config.pin_memory
         self.scaler = GradScaler(device=self.device)
-        self.enable_amp = True if torch.cuda.is_available() else False
+        self.enable_amp = True if self.device == "cuda" else False
         self.report_to_wandb = config.report_to_wandb
         self.wandb_project = config.wandb_project
+        self.distributed_backend = config.distributed_backend
 
         torch.manual_seed(self.seed) if self.seed is not None else None
         torch.cuda.manual_seed(self.seed) if self.seed is not None else None
@@ -91,33 +92,70 @@ class Trainer:
                 eps=self.lr_epsilon
                 )
 
-        self.train_data = self.dataloader(config.train_data)
-        self.val_data = self.dataloader(config.val_data)
-        self.test_data = self.dataloader(config.test_data)
-
         
-        self.logs = {
-            "epoch": 0,
-            "global_step": 0,
-            "train_loss": [],
-            "val_loss": [],
-            "best_loss": float("inf"),
-        }
+        self.logs = {"epoch": 0, "global_step": 0, "train_loss": [],"val_loss": [],"best_loss": float("inf")}
         self.model.to(self.device)
 
         if self.report_to_wandb:
             wandb.init(project=self.wandb_project, name=self.model_name)
-            #wandb.config.update(asdict(self.config))
             wandb.watch(self.model, log="all", log_graph=False, log_freq=self.logging_steps)
 
         self.save_config()
 
 
-    def train(self):
+        #==================================================================================================
+        if self.distributed_backend == "ddp":
+            self.model = self._trigger_ddp()
+        elif self.distributed_backend == "dp":
+            self.model = self._trigger_dp()
+        elif self.distributed_backend is None:
+            pass
+        else:
+            raise ValueError(f"Invalid distributed backend: {self.distributed_backend}, must be one of ['ddp', 'dp', None]")
+
+        self.train_data = self._created_dataloader(config.train_data)
+        self.val_data = self._created_dataloader(config.val_data)
+        self.test_data = self._created_dataloader(config.test_data)
+    
+    def _trigger_ddp(self) -> torch.nn.Module:
+        """
+        This method will be trigger for DistributedDataParallel (DDP) training.
+        """
+        self.rank = int(os.environ["RANK"])
+        self.world_size = int(os.environ["WORLD_SIZE"])
+        self.local_rank = int(os.environ["LOCAL_RANK"])
+        torch.cuda.set_device(self.local_rank)
+        self.device = torch.device(self.device, self.local_rank)
+        torch.distributed.init_process_group(
+            backend="nccl",
+            init_method="env://"
+        )
+        self.model = torch.nn.parallel.DistributedDataParallel(
+            self.model, 
+            device_ids=[self.local_rank], 
+            output_device=self.local_rank,
+            find_unused_parameters=True
+            )
+        return self.model
+    
+
+    def _trigger_dp(self) -> torch.nn.Module:
+        """
+        This method will be trigger for DataParallel (DP) training.
+        """
+        device_count = torch.cuda.device_count()
+        self.model = torch.nn.DataParallel(self.model, device_ids=list(range(device_count)))
+        return self.model
+
+        
+
+    def train(self) -> None:
         self.model.train()
         global_step = 1 if self.logs["global_step"] == 0 else self.logs["global_step"]
         accumulated_loss = 0.0  # Track accumulated loss
         start_epoch = self.logs["epoch"]
+        device_type = self.device.type if isinstance(self.device, torch.device) else self.device
+
         with tqdm(total=self.epochs, desc=f"Training | Step {global_step}") as pbar:
             pbar.update(self.logs["epoch"]) if self.logs["epoch"] > 0 else None
             for epoch in range(start_epoch, self.epochs + 1):
@@ -125,7 +163,7 @@ class Trainer:
                 for batch in self.train_data:
                     pbar.set_description(f"Training | Step {global_step}")
                     with autocast(
-                        device_type=self.device, 
+                        device_type=device_type, 
                         dtype=self.precision, 
                         enabled=self.enable_amp
                         ):
@@ -186,17 +224,25 @@ class Trainer:
 
         if self.report_to_wandb:
             wandb.finish()
+        if self.distributed_backend == "ddp":
+            torch.distributed.destroy_process_group()
+
     
     def from_checkpoint(self, checkpoint_path: str) -> None:
         if os.path.exists(checkpoint_path):
-            model_file = os.path.join(checkpoint_path, "pytorch_model.pt")  # Specify the model file
-            self.model.load_state_dict(torch.load(model_file))  # Load the model state dict
+            model_file = os.path.join(checkpoint_path, "pytorch_model.pt")
+            state_dict = torch.load(model_file)
+
+            # Handle DDP/DP prefix if necessary
+            if self.distributed_backend == "ddp" or self.distributed_backend == "dp":
+                state_dict = {f"module.{k}": v for k, v in state_dict.items()}
+
+            self.model.load_state_dict(state_dict)  # Load the model state dict
             self.optimizer.load_state_dict(torch.load(os.path.join(checkpoint_path, "optimizer.pt")))
             self.scaler.load_state_dict(torch.load(os.path.join(checkpoint_path, "scaler.pt")))
             self.logs = torch.load(os.path.join(checkpoint_path, "logs.pt"))
 
             print(f"Model loaded from {checkpoint_path}")
-            #self.train()
         else:
             raise ValueError(f"Checkpoint path {checkpoint_path} does not exist")
 
@@ -213,11 +259,10 @@ class Trainer:
 
     def evaluate(self) -> None:
         self.model.eval()
-        device_type = self.device if isinstance(self.device, str) else self.device.type
         for batch in self.val_data:
             with torch.no_grad():
                 with autocast(
-                    device_type=device_type, 
+                    device_type=self.device, 
                     dtype=self.precision, 
                     enabled=self.enable_amp
                     ):
@@ -225,33 +270,56 @@ class Trainer:
                     self.logs["val_loss"].append(loss.item())
 
 
-    def dataloader(self, dataset: Union[Any, DataLoader]) -> DataLoader:
+    def _created_dataloader(self, dataset: Union[Any, DataLoader]) -> DataLoader:
         if dataset is None:
             return None
+        
         if isinstance(dataset, DataLoader):
             return dataset
-        else:
-            return DataLoader(
-                dataset=dataset, 
-                batch_size=self.batch_size, 
-                num_workers=self.num_workers, 
-                pin_memory=self.pin_memory,
-                shuffle=self.shuffle_train_data,
-                collate_fn=self.collate_fn,
-                drop_last=True,
-            )
+
+        sampler = None
+        shuffle = self.shuffle_train_data
+
+        if self.distributed_backend == "ddp":
+            if not torch.distributed.is_initialized():
+                raise RuntimeError("Distributed training is not initialized.")
+            
+            if not hasattr(dataset, "__len__"):
+                warnings.warn(
+                    f"Dataset {dataset.__class__.__name__} does not have __len__ attribute. Skipping `DistributedSampler`."
+                )
+            else:
+                sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+                shuffle = False  # Shuffling is handled by the sampler
+
+
+        return torch.utils.data.DataLoader(
+            dataset=dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            pin_memory=self.pin_memory,
+            shuffle=shuffle,
+            sampler=sampler,
+            collate_fn=self.collate_fn,
+            drop_last=True,
+        )
         
 
-    def save(self):
+    def save(self) -> None:
+        if self.distributed_backend == "ddp" or self.distributed_backend == "dp":
+            model_state_dict = self.model.module.state_dict()
+        else:
+            model_state_dict = self.model.state_dict()
+
         current_loss = self.logs["train_loss"][-1]
         if current_loss < self.logs["best_loss"]:
             self.logs["best_loss"] = current_loss
-            torch.save(self.model.state_dict(), f"{self.model_name}/pytorch_model.pt")  
+            torch.save(model_state_dict, f"{self.model_name}/pytorch_model.pt")  
 
             # Save all snapshots in the checkpoint directory
             ckpt_dir = os.path.join(self.model_name, f"checkpoint-{self.logs['global_step']}")
             os.makedirs(ckpt_dir, exist_ok=True)
-            torch.save(self.model.state_dict(), os.path.join(ckpt_dir, "pytorch_model.pt"))
+            torch.save(model_state_dict, os.path.join(ckpt_dir, "pytorch_model.pt"))
             torch.save(self.optimizer.state_dict(), os.path.join(ckpt_dir, "optimizer.pt"))
             torch.save(self.scaler.state_dict(), os.path.join(ckpt_dir, "scaler.pt"))
             torch.save(self.logs, os.path.join(ckpt_dir, "logs.pt"))
@@ -267,7 +335,7 @@ class Trainer:
                     shutil.rmtree(os.path.join(self.model_name, checkpoint))
 
 
-    def save_config(self):
+    def save_config(self) -> None:
         # Save config in a yaml file
         trainer_config_dict = {}
         for key, value in asdict(self.config).items():
