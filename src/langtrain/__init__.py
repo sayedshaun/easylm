82d35d0e6119f2a1a257f021d5579b62b34fa225
sayedshaun <sayedshaun4@gmail.com>
2025-04-29 22:35:13 +0600

    now user can direct import classes from package

--- src/langtrain/__init__.py
+++ src/langtrain/__init__.py
@@ -1,6 +1,7 @@
 # src/__init__.py
 from langtrain.config import *
-from langtrain.tokenizer import *
+from langtrain.tokenizer import Tokenizer
 from langtrain.nn import *
 from langtrain.model import *
 from langtrain.data import *
+from langtrain.trainer import Trainer
