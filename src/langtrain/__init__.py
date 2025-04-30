8bdc38f66a0ec1116d764ae799a1428d3c0b79dc
sayedshaun <sayedshaun4@gmail.com>
2025-04-30 15:56:13 +0600

    added dynamic version to init file

--- src/langtrain/__init__.py
+++ src/langtrain/__init__.py
@@ -1,1 +1,13 @@
+import tomli
+import os
+
+def get_version():
+    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "..", "pyproject.toml")
+    with open(pyproject_path, "rb") as f:
+        pyproject_data = tomli.load(f)
+        return pyproject_data["project"]["version"]
+
+if "__version__" not in globals():
+    __version__ = get_version()
+
 from langtrain import config, data, model, trainer, tokenizer, utils
