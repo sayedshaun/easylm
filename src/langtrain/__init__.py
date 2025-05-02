ea268691b404441d2e43ebe9c7ef3c2775a8803e
sayedshaun <sayedshaun4@gmail.com>
2025-05-02 08:45:42 +0600

    added package version

--- src/langtrain/__init__.py
+++ src/langtrain/__init__.py
@@ -1,14 +1,3 @@
-import tomli
-import os
-
-def get_version():
-    pyproject_path = os.path.join(os.path.dirname(__file__), "../../pyproject.toml")
-    pyproject_path = os.path.abspath(pyproject_path)
-    if not os.path.exists(pyproject_path):
-        raise FileNotFoundError(f"pyproject.toml not found at: {pyproject_path}")
-    with open(pyproject_path, "rb") as f:
-        return tomli.load(f)["project"]["version"]
-
-__version__ = get_version()
-
 from langtrain import config, data, model, trainer, tokenizer, utils, nn
+
+__version__ = "0.0.3"
