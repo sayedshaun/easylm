28d5e3b5a22719fe0e2e0c63b185cdff78df6e41
sayedshaun <sayedshaun4@gmail.com>
2025-05-03 09:13:37 +0600

    updated script for local build and testing

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 echo "Installing the package..."
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-pip install git+https://github.com/sayedshaun/langtrain.git
+
+pip install -e .
 
 echo "Running tests with pytest..."
 pytest tests/
@@ -11,3 +12,8 @@ rm -rf tests/__pycache__
 rm -rf tests/pretrained_model
 rm -rf .pytest_cache
 rm -rf tests/test_weights
+
+# Remove all local build files
+rm -rf build
+rm -rf dist
+rm -rf src/langtrain.egg-info
